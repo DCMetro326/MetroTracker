7c3c1773d8cd5462aab2697095c67919ef56a6c8
DCMetro326 <davidand326@gmail.com>
2025-12-10 00:05:58 -0500

    Update brentwoodScript.js

--- brentwoodScript.js
+++ brentwoodScript.js
@@ -26,7 +26,7 @@ for (let t = 10; t <= 12; t++) {
 }
 
 // Track 13 : 2 trains
-T.buildLeftRow(left, 4, "13");
+T.buildLeftRow(left, 2, "13");
 
 // ===================================================
 // RIGHT SIDE TRACKS

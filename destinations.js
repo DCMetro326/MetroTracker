49dce10b12ba6810b5e3b1e3c88fc7ab4f08917f
DCMetro326 <davidand326@gmail.com>
2025-12-10 23:10:50 -0500

    Create destinations.js

---
+++ destinations.js
@@ -0,0 +1,24 @@
+// destinations.js
+
+window.destinations = {
+    "12": {
+        color: "red",
+        stationName: "Shady Grove",
+        displayName: "Shady Grove"
+    },
+    "13": {
+        color: "red",
+        stationName: "Glenmont",
+        displayName: "Glenmont"
+    },
+    "16": {
+        color: "blue",
+        stationName: "Franconia-Springfield",
+        displayName: "Franconia-Springfield"
+    },
+    "20": {
+        color: "orange",
+        stationName: "New Carrollton",
+        displayName: "New Carrollton"
+    },
+};

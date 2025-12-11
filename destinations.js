7d4b8df9fc1ec66a0cdcf7b05e7988633751fc06
DCMetro326 <davidand326@gmail.com>
2025-12-11 00:26:06 -0500

    Update destinations.js

--- destinations.js
+++ destinations.js
@@ -4,21 +4,76 @@ window.destinations = {
     '12': {
         color: "red",
         stationName: "Shady Grove",
-        displayName: "Shady Grove"
+        displayName: "A15"
     },
     '13': {
         color: "red",
         stationName: "Glenmont",
-        displayName: "Glenmont"
+        displayName: "B11"
     },
     '16': {
         color: "blue",
         stationName: "Franconia-Springfield",
-        displayName: "Franconia-Springfield"
+        displayName: "J03"
     },
     '20': {
         color: "orange",
         stationName: "New Carrollton",
-        displayName: "New Carrollton"
+        displayName: "D13"
+    },
+    '23': {
+        color: "orange",
+        stationName: "Vienna",
+        displayName: "K08"
+    },
+    '31': {
+        color: "yellow",
+        stationName: "Huntington",
+        displayName: "C15"
+    },
+    '44': {
+        color: "green",
+        stationName: "Greenbelt",
+        displayName: "E10"
+    },
+    '51': {
+        color: "silver",
+        stationName: "Downtown Largo",
+        displayName: "G05"
+    },
+    '68': {
+        color: "silver",
+        stationName: "Ashburn",
+        displayName: "N12"
+    },
+    '72': {
+        color: "blue",
+        stationName: "Downtown Largo",
+        displayName: "G05"
+    },
+    '92': {
+        color: "black",
+        stationName: "Greenbelt Yard",
+        displayName: "E99"
+    },
+    '93': {
+        color: "black",
+        stationName: "Dulles Yard",
+        displayName: "N99"
+    },
+    '94': {
+        color: "black",
+        stationName: "West Falls Church Yard",
+        displayName: "K99"
+    },
+    '96': {
+        color: "black",
+        stationName: "Branch Avenue Yard",
+        displayName: "F99"
+    },
+    '99': {
+        color: "black",
+        stationName: "Alexandria Yard",
+        displayName: "C99"
     },
 };

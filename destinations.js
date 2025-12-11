a30556f4eac89fb38fe2b442c939cef52b6e94a3
DCMetro326 <davidand326@gmail.com>
2025-12-10 23:17:07 -0500

    Update destinations.js

--- destinations.js
+++ destinations.js
@@ -1,22 +1,22 @@
 // destinations.js
 
 window.destinations = {
-    "12": {
+    '12': {
         color: "red",
         stationName: "Shady Grove",
         displayName: "Shady Grove"
     },
-    "13": {
+    '13': {
         color: "red",
         stationName: "Glenmont",
         displayName: "Glenmont"
     },
-    "16": {
+    '16': {
         color: "blue",
         stationName: "Franconia-Springfield",
         displayName: "Franconia-Springfield"
     },
-    "20": {
+    '20': {
         color: "orange",
         stationName: "New Carrollton",
         displayName: "New Carrollton"

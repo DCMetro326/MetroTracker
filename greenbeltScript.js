7e7e4629aec60285aede9f2eb9193cec36f306a6
DCMetro326 <davidand326@gmail.com>
2026-01-23 02:50:32 -0500

    Update greenbeltScript.js

--- greenbeltScript.js
+++ greenbeltScript.js
@@ -5,7 +5,12 @@ const T = window.YardTools;
 const left = document.getElementById("leftGrid");
 const right = document.getElementById("rightGrid");
 
-// Build left rows
+
+// ===================================================
+// LEFT SIDE TRACKS
+// ===================================================
+
+// Tracks 1n-7n,P1,P2,S1,S2
 T.buildLeftRow(left, 4, "1n");
 T.buildLeftRow(left, 7, "2n");
 T.buildLeftRow(left, 5, "3n");
@@ -20,30 +25,39 @@ T.buildLeftRow(left, 4, "S1");
 T.buildLeftRow(left, 4, "S2");
 
 
-// Build right rows
+// ===================================================
+// RIGHT SIDE TRACKS
+// ===================================================
+
+// Tracks 20-17
 T.buildRightRow(right, 4, 20);
 T.buildRightRow(right, 5, 19);
 T.buildRightRow(right, 5, 18);
 T.buildRightRow(right, 3, 17);
 
+//Tracks 16-1: 8 trains each
 for (let r = 16; r >= 1; r--) {
     T.buildRightRow(right, 8, r);
 }
 
-// Mapping
+
+// ===================================================
+// TRACK MAP
+// ===================================================
+
 const map = {
     "1N":"1n","2N":"2n","3N":"3n","4N":"4n","5N":"5n",
     "5AN":"5an","6N":"6n","7N":"7n",
     "P1":"P1","P2":"P2","S1":"S1","S2":"S2"
 };
 
-// numeric tracks 1–20
+// Numeric tracks 1–20
 for (let i = 1; i <= 20; i++) {
-    const normal = i.toString();        // "1"
-    const padded = normal.padStart(2, "0"); // "01"
+    const normal = i.toString();            // "1"
+    const padded2 = normal.padStart(2, "0"); // "01"
 
     map[normal] = i;
-    map[padded] = i;
+    map[padded2] = i;
 }
 
 T.loadWMATA("Greenbelt Yard", map);

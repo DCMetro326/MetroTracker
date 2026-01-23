52ae99019a38c2cd93060db9551cd0d10d7c1f7f
DCMetro326 <davidand326@gmail.com>
2026-01-23 03:10:15 -0500

    Update westfallschurchScript.js

--- westfallschurchScript.js
+++ westfallschurchScript.js
@@ -7,55 +7,55 @@ const right = document.getElementById("rightGrid");
 
 
 // ===================================================
-// LEFT SIDE
+// LEFT SIDE TRACKS
 // ===================================================
 
-// Lettered tracks
+// Tracks 6h,6g: 3 trains each
 T.buildLeftRow(left, 3, "6h");
 T.buildLeftRow(left, 3, "6g");
 
-// Numeric 16 → 21 (4 trains each)
+// Tracks 16-21: 4 trains each
 for (let t = 16; t <= 21; t++) {
     T.buildLeftRow(left, 4, t.toString());
 }
 
-// T1 - 4 trains?
+// Track T1: 4 trains?
 T.buildLeftRow(left, 4, "T1");
 
-// S1-3 - 3 trains?
+// Tracks S1,S2,S3: 3 trains each?
 T.buildLeftRow(left, 3, "S1");
 T.buildLeftRow(left, 3, "S2");
 T.buildLeftRow(left, 3, "S3");
 
 
 // ===================================================
-// RIGHT SIDE
+// RIGHT SIDE TRACKS
 // ===================================================
 
-// Special 1-series
+// Tracks 1c,1b,1a
 T.buildRightRow(right, 5, "1c");
 T.buildRightRow(right, 5, "1b");
 T.buildRightRow(right, 6, "1a");
 
-// Tracks 1 → 4: 6 trains each
+// Tracks 1-4: 6 trains each
 for (let t = 1; t <= 4; t++) {
     T.buildRightRow(right, 6, t.toString());
 }
 
-// 6-series (lettered)
+// Tracks 6e,6d,6c,6b,6a: 4 trains each
 T.buildRightRow(right, 4, "6e");
 T.buildRightRow(right, 4, "6d");
 T.buildRightRow(right, 4, "6c");
 T.buildRightRow(right, 4, "6b");
 T.buildRightRow(right, 4, "6a");
 
-// Track 6 (numeric): 4 trains
+// Track 6: 4 trains
 T.buildRightRow(right, 4, "6");
 
 // Track 7: 5 trains
 T.buildRightRow(right, 5, "7");
 
-// Tracks 8 → 10: 6 trains each
+// Tracks 8-10: 6 trains each
 for (let t = 8; t <= 10; t++) {
     T.buildRightRow(right, 6, t.toString());
 }
@@ -65,20 +65,21 @@ T.buildRightRow(right, 7, "11");
 
 
 // ===================================================
-// TRACK MAP — map incoming WMATA API names
+// TRACK MAP
 // ===================================================
 
 const map = {};
 
 // Numeric tracks 1–21
 for (let i = 1; i <= 21; i++) {
-    const normal = i.toString();            // "7"
-    const padded2 = normal.padStart(2, "0"); // "07"
+    const normal = i.toString();            // "1"
+    const padded2 = normal.padStart(2, "0"); // "01"
 
     map[normal] = normal;
     map[padded2] = normal;
 }
 
+// Special tracks
 map["6A"] = "6a";
 map["6B"] = "6b";
 map["6C"] = "6c";
@@ -94,10 +95,5 @@ map["S1"] = "S1";
 map["S2"] = "S2";
 map["S3"] = "S3";
 
-
-
-// ===================================================
-// LOAD WMATA DATA FOR THIS YARD
-// ===================================================
 
 T.loadWMATA("West Falls Church Yard", map);

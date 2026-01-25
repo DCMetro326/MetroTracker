aab18ebe18a9701be8c593b9c6fd07cec0f617e2
DCMetro326 <davidand326@gmail.com>
2026-01-24 23:42:21 -0500

    Update terminalsScript.js

--- terminalsScript.js
+++ terminalsScript.js
@@ -2,55 +2,178 @@
 
 const T = window.YardTools;
 
-const vienna = document.getElementById("vienna");
 const shadyGrove = document.getElementById("shadyGrove");
+const glenmont = document.getElementById("glenmont");
+const huntington = document.getElementById("huntington");
+const newCarrollton = document.getElementById("newCarrollton");
+const mtVernonSq = document.getElementById("mtVernonSq");
 const greenbelt = document.getElementById("greenbelt");
+const branchAve = document.getElementById("branchAve");
+const largo = document.getElementById("largo");
+const franconia = document.getElementById("franconia");
+const vienna = document.getElementById("vienna");
+const ashburn = document.getElementById("ashburn");
+const grosvenor = document.getElementById("grosvenor");
+const silverSpring = document.getElementById("silverSpring");
+const wiehle = document.getElementById("wiehle");
 
 
 // ===================================================
 // GRID TRACKS
 // ===================================================
 
-// Vienna
-T.buildRightRow(vienna, 4, "Vienna1");
-T.buildRightRow(vienna, 4, "Vienna2");
-
 // Shady Grove
-T.buildRightRow(shadyGrove, 4, "ShadyGrove1");
-T.buildRightRow(shadyGrove, 4, "ShadyGrove2");
+T.buildRightRow(shadyGrove, 4, "A1");
+T.buildRightRow(shadyGrove, 4, "A2");
+
+// Glenmont
+T.buildRightRow(glenmont, 4, "B1");
+T.buildRightRow(glenmont, 4, "B2");
+
+// Huntington
+T.buildRightRow(huntington, 4, "C1");
+T.buildRightRow(huntington, 4, "C2");
+
+// New Carrollton
+T.buildRightRow(newCarrollton, 4, "D1");
+T.buildRightRow(newCarrollton, 4, "D2");
+
+// Mt Vernon Sq
+T.buildRightRow(newCarrollton, 4, "E1");
+T.buildRightRow(newCarrollton, 4, "E2");
 
 // Greenbelt
-T.buildRightRow(greenbelt, 4, "Greenbelt1");
-T.buildRightRow(greenbelt, 4, "Greenbelt2");
+T.buildRightRow(greenbelt, 4, "E1 ");
+T.buildRightRow(greenbelt, 4, "E2 ");
+
+// Branch Ave
+T.buildRightRow(branchAve, 4, "F1");
+T.buildRightRow(branchAve, 4, "F2");
+
+// Downtown Largo
+T.buildRightRow(largo, 4, "G1");
+T.buildRightRow(largo, 4, "G2");
+
+// Franconia
+T.buildRightRow(franconia, 4, "J1");
+T.buildRightRow(franconia, 4, "J2");
+
+// Vienna
+T.buildRightRow(vienna, 4, "K1");
+T.buildRightRow(vienna, 4, "K2");
+
+// Ashburn
+T.buildRightRow(ashburn, 4, "N1");
+T.buildRightRow(ashburn, 4, "N2");
+
+// Grosvenor
+T.buildRightRow(grosvenor, 4, "A1 ");
+T.buildRightRow(grosvenor, 4, "A2 ");
+
+// Silver Spring
+T.buildRightRow(silverSpring, 4, "B1 ");
+T.buildRightRow(silverSpring, 4, "B2 ");
+
+// Wiehle
+T.buildRightRow(wiehle, 4, "N1 ");
+T.buildRightRow(wiehle, 4, "N2 ");
 
 
 // ===================================================
 // TRACK MAP
 // ===================================================
 
-const viennaMap = {};
 const shadyGroveMap = {};
+const glenmontMap = {};
+const huntingtonMap = {};
+const newCarrolltonMap = {};
+const mtVernonSqMap = {};
 const greenbeltMap = {};
+const branchAveMap = {};
+const largoMap = {};
+const franconiaMap = {};
+const viennaMap = {};
+const ashburnMap = {};
+const grosvenorMap = {};
+const silverSpringMap = {};
+const wiehleMap = {};
 
 function addTrack(map, raw, mapped) {
     map[raw] = mapped;
     map[raw.padStart(2, "0")] = mapped;
 }
 
-// Vienna
-addTrack(viennaMap, "1", "Vienna1");
-addTrack(viennaMap, "2", "Vienna2");
-
 // Shady Grove
 addTrack(shadyGroveMap, "1", "ShadyGrove1");
 addTrack(shadyGroveMap, "2", "ShadyGrove2");
+
+// Glenmont
+addTrack(glenmontMap, "1", "Glenmont1");
+addTrack(glenmontMap, "2", "Glenmont2");
+
+// Huntington
+addTrack(huntingtonMap, "1", "Huntington1");
+addTrack(huntingtonMap, "2", "Huntington2");
+
+// New Carrollton
+addTrack(newCarrolltonMap, "1", "NewCarrollton1");
+addTrack(newCarrolltonMap, "2", "NewCarrollton2");
+
+// Mt Vernon Sq
+addTrack(mtVernonSqMap, "1", "MtVernonSq1");
+addTrack(mtVernonSqMap, "2", "MtVernonSq2");
 
 // Greenbelt
 addTrack(greenbeltMap, "1", "Greenbelt1");
 addTrack(greenbeltMap, "2", "Greenbelt2");
 
+// Branch Ave
+addTrack(branchAveMap, "1", "BranchAve1");
+addTrack(branchAveMap, "2", "BranchAve2");
+
+// Largo
+addTrack(largoMap, "1", "Largo1");
+addTrack(largoMap, "2", "Largo2");
+
+// Franconia
+addTrack(franconiaMap, "1", "Franconia1");
+addTrack(franconiaMap, "2", "Franconia2");
+
+// Vienna
+addTrack(viennaMap, "1", "Vienna1");
+addTrack(viennaMap, "2", "Vienna2");
+
+// Ashburn
+addTrack(ashburnMap, "1", "Ashburn1");
+addTrack(ashburnMap, "2", "Ashburn2");
+
+// Grosvenor
+addTrack(grosvenorMap, "1", "Grosvenor1");
+addTrack(grosvenorMap, "2", "Grosvenor2");
+
+// Silver Spring
+addTrack(silverSpringMap, "1", "SilverSpring1");
+addTrack(silverSpringMap, "2", "SilverSpring2");
+
+// Wiehle
+addTrack(wiehleMap, "1", "Wiehle1");
+addTrack(wiehleMap, "2", "Wiehle2");
+
 T.clearCells();
 
-T.loadWMATA("Vienna", viennaMap, { clear: false });
 T.loadWMATA("Shady Grove", shadyGroveMap, { clear: false });
+T.loadWMATA("Glenmont", glenmontMap, { clear: false });
+T.loadWMATA("Huntington", huntingtonMap, { clear: false });
+T.loadWMATA("New Carrollton", newCarrolltonMap, { clear: false });
+T.loadWMATA("Mt Vernon Sq", mtVernonSqMap, { clear: false });
 T.loadWMATA("Greenbelt", greenbeltMap, { clear: false });
+T.loadWMATA("Branch Ave", branchAveMap, { clear: false });
+T.loadWMATA("Downtown Largo", largoMap, { clear: false });
+T.loadWMATA("Franconia - Springfield", franconiaMap, { clear: false });
+T.loadWMATA("Vienna", viennaMap, { clear: false });
+T.loadWMATA("Ashburn", ashburnMap, { clear: false });
+T.loadWMATA("Grosvenor - Strathmore", grosvenorMap, { clear: false });
+T.loadWMATA("Silver Spring", silverSpringMap, { clear: false });
+T.loadWMATA("Wiehle-Reston East", wiehleMap, { clear: false });
+
+

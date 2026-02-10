031c218db2237b98d0e98158745df059d2021fed
DCMetro326 <davidand326@gmail.com>
2026-02-09 23:11:19 -0500

    Update terminalsScript.js

--- terminalsScript.js
+++ terminalsScript.js
@@ -53,11 +53,11 @@ T.buildRightRow(branchAve, 4, "F2");
 // Downtown Largo
 T.buildRightRow(largo, 4, "G1");
 T.buildRightRow(largo, 4, "G2");
-T.buildRightRow(right, 6, "41");
-T.buildRightRow(right, 8, "42");
-T.buildRightRow(right, 5, "43");
-T.buildRightRow(right, 3, "44");
-T.buildRightRow(right, 3, "45");
+T.buildRightRow(largo, 6, "41");
+T.buildRightRow(largo, 8, "42");
+T.buildRightRow(largo, 5, "43");
+T.buildRightRow(largo, 3, "44");
+T.buildRightRow(largo, 3, "45");
 
 // Franconia
 T.buildRightRow(franconia, 4, "J1");
